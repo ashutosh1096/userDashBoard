39c2d86ef022e284a67e83ab4a98b5de20121c9b
Ashutosh Singh <ashutoshsingh@192.168.1.4>
2025-12-06 13:01:49 +0530

    Removed ProjectsContext.js and updated project code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,36 +11,43 @@ import "./style.css";
 
 function App() {
   const loading = useSelector(state => state.auth && state.auth.loading);
+
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app-layout">
         {loading && (
           <div className="loader-overlay" aria-hidden>
             <div className="loader" />
           </div>
         )}
+
         <Navbar />
+
         <main className="main-area">
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+
             <Route
               path="/"
               element={<ProtectedRoute><Dashboard /></ProtectedRoute>}
             />
+
             <Route
               path="/users"
               element={<ProtectedRoute><Users /></ProtectedRoute>}
             />
+
             <Route
               path="/projects"
               element={<ProtectedRoute><Projects /></ProtectedRoute>}
             />
+
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
